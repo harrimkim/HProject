c74a596dfef352490457c0a488c6a3cabc6c333b
harrimkim <harrimkim@gmail.com>
2020-02-23 16:30:41 +0900

    메인 클래스 이름 변경

--- src/main/java/com/example/hproject/HProjectApplication.java
+++ src/main/java/com/example/hproject/HProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HprojectApplication {
+public class HProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HprojectApplication.class, args);
+        SpringApplication.run(HProjectApplication.class, args);
     }
 
 }
--- src/test/java/com/example/hproject/HProjectApplicationTests.java
+++ src/test/java/com/example/hproject/HProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HprojectApplicationTests {
+class HProjectApplicationTests {
 
     @Test
     void contextLoads() {
